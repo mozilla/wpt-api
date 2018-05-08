7824f8de10519f6fde0a2b81650c3f684db22a4d
Stephen Donner <stephen.donner@gmail.com>
2018-05-08 15:54:23 -0700

    Try importing selected JSON into Python object, and sending to statsd

--- send_to_datadog.py
+++ send_to_datadog.py
@@ -1,3 +1,5 @@
+import json
+
 from datadog import initialize, api
 from datadog import statsd
 
@@ -5,6 +7,10 @@ from datadog import statsd
 options = {'statsd_host': 'localhost',
            'statsd_port': '8125'}
 
+with open ('stats.json') as json_data:
+    statsd_data = json.load(json_data)
+    print(statsd_data)
+
 initialize(**options)
 
-statsd.gauge('wpt.median.firstView.TTFB', '429')
+statsd.gauge(statsd_data)
