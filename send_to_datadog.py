52dbf5865397397f63c2703d0956019c904f3171
Stephen Donner <stephen.donner@gmail.com>
2018-05-10 14:38:17 -0700

    Correctly wrap print statements in single parentheses

--- send_to_datadog.py
+++ send_to_datadog.py
@@ -11,14 +11,14 @@ with open ('fxa-homepage.json') as json_data:
     loaded_json = json.load(json_data)
     # dumped_json = json.dumps(loaded_json)
     # print(dumped_json)
-    print "data.median.firstView.TTFB {}".format(loaded_json["data"]["median"]["firstView"]["TTFB"])
-    print "data.median.firstView.render {}".format(loaded_json["data"]["median"]["firstView"]["render"])
-    print "data.median.firstView.SpeedIndex {}".format(loaded_json["data"]["median"]["firstView"]["SpeedIndex"])
-    print "data.median.firstView.bytesInDoc {}".format(loaded_json["data"]["median"]["firstView"]["bytesInDoc"])
-    print "data.median.firstView.visualComplete {}".format(loaded_json["data"]["median"]["firstView"]["visualComplete"])
-    print "data.median.firstView.requestsFull {}".format(loaded_json["data"]["median"]["firstView"]["requestsFull"])
+    print("data.median.firstView.TTFB {}".format(loaded_json["data"]["median"]["firstView"]["TTFB"]))
+    print("data.median.firstView.render {}".format(loaded_json["data"]["median"]["firstView"]["render"]))
+    print("data.median.firstView.SpeedIndex {}".format(loaded_json["data"]["median"]["firstView"]["SpeedIndex"]))
+    print("data.median.firstView.bytesInDoc {}".format(loaded_json["data"]["median"]["firstView"]["bytesInDoc"]))
+    print("data.median.firstView.visualComplete {}".format(loaded_json["data"]["median"]["firstView"]["visualComplete"]))
+    print("data.median.firstView.requestsFull {}".format(loaded_json["data"]["median"]["firstView"]["requestsFull"]))
 
-print('wpt.median.firstView.TTFB', '{}'.format(loaded_json["data"]["median"]["firstView"]["TTFB"]))
+# print('wpt.median.firstView.TTFB', '{}'.format(loaded_json["data"]["median"]["firstView"]["TTFB"]))
 
 
 initialize(**options)
