257f5732717b5af43519266990e1207c9cf5e5af
Stephen Donner <stephen.donner@gmail.com>
2018-05-09 23:35:09 -0700

    Add dog.gauge() call

--- send_to_datadog.py
+++ send_to_datadog.py
@@ -26,5 +26,7 @@ initialize(**options)
 statsd.gauge('wpt.median.firstView.TTFB.fakeValue', '42', sample_rate=1)
 statsd.gauge('wpt.median.firstView.TTFB', loaded_json["data"]["median"]["firstView"]["TTFB"], sample_rate=1)
 
+dog.gauge('wpt.median.firstView.TTFB.fakeValue', '42', sample_rate=1)
+
 # statsd.set('wpt.median.firstView.TTFB.fakeValue', '42')
 # statsd.set('wpt.median.firstView.TTFB', loaded_json["data"]["median"]["firstView"]["TTFB"])
