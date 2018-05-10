4dfeac781012c77ef0456b43d3cac3da3c8832a6
Stephen Donner <stephen.donner@gmail.com>
2018-05-09 17:06:19 -0700

    First stab at sending real metrics, with field names prepended

--- send_to_datadog.py
+++ send_to_datadog.py
@@ -7,10 +7,22 @@ from datadog import statsd
 options = {'statsd_host': 'localhost',
            'statsd_port': '8125'}
 
-with open ('stats.json') as json_data:
-    statsd_data = json.load(json_data)
-    print(statsd_data)
+with open ('fxa-homepage.json') as json_data:
+    loaded_json = json.load(json_data)
+    # dumped_json = json.dumps(loaded_json)
+    # print(dumped_json)
+    print "data.median.firstView.TTFB {}".format(loaded_json["data"]["median"]["firstView"]["TTFB"])
+    print "data.median.firstView.render {}".format(loaded_json["data"]["median"]["firstView"]["render"])
+    print "data.median.firstView.SpeedIndex {}".format(loaded_json["data"]["median"]["firstView"]["SpeedIndex"])
+    print "data.median.firstView.bytesInDoc {}".format(loaded_json["data"]["median"]["firstView"]["bytesInDoc"])
+    print "data.median.firstView.visualComplete {}".format(loaded_json["data"]["median"]["firstView"]["visualComplete"])
+    print "data.median.firstView.requestsFull {}".format(loaded_json["data"]["median"]["firstView"]["requestsFull"])
+
+print('wpt.median.firstView.TTFB', '{}'.format(loaded_json["data"]["median"]["firstView"]["TTFB"]))
+
 
 initialize(**options)
 
-statsd.gauge(statsd_data)
+statsd.gauge('wpt.median.firstView.TTFB.fakeValue', '42')
+statsd.gauge('wpt.median.firstView.TTFB', '{}'.format(loaded_json["data"]["median"]["firstView"]["TTFB"]))
+statsd.gauge('wpt.median.firstView.TTFB', loaded_json["data"]["median"]["firstView"]["TTFB"])
