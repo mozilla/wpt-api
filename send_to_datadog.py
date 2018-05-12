a2889ed2dfd6f1d574e53d8889c5ae04e92654cb
Stephen Donner <stephen.donner@gmail.com>
2018-05-11 17:30:01 -0700

    Round out the rest of the metrics we care about; remove now-unused jq-filter file

--- send_to_datadog.py
+++ send_to_datadog.py
@@ -9,18 +9,25 @@ options = {'statsd_host': 'localhost',
 
 with open ('fxa-homepage.json') as json_data:
     loaded_json = json.load(json_data)
-    # dumped_json = json.dumps(loaded_json)
-    # print(dumped_json)
-    # print("data.median.firstView.TTFB {}".format(loaded_json["data"]["median"]["firstView"]["TTFB"]))
-    # print("data.median.firstView.render {}".format(loaded_json["data"]["median"]["firstView"]["render"]))
-    # print("data.median.firstView.SpeedIndex {}".format(loaded_json["data"]["median"]["firstView"]["SpeedIndex"]))
-    # print("data.median.firstView.bytesInDoc {}".format(loaded_json["data"]["median"]["firstView"]["bytesInDoc"]))
-    # print("data.median.firstView.visualComplete {}".format(loaded_json["data"]["median"]["firstView"]["visualComplete"]))
-    # print("data.median.firstView.requestsFull {}".format(loaded_json["data"]["median"]["firstView"]["requestsFull"]))
+
+    '''
+    Just leaving the example below in, for my own, local reference
+    print("data.median.firstView.TTFB {}".format(loaded_json["data"]["median"]["firstView"]["TTFB"]))
+    '''
 
 
 initialize(**options)
 
 TTFB = loaded_json["data"]["median"]["firstView"]["TTFB"]
+render = loaded_json["data"]["median"]["firstView"]["render"]
+SpeedIndex = loaded_json["data"]["median"]["firstView"]["SpeedIndex"]
+bytesInDoc = loaded_json["data"]["median"]["firstView"]["bytesInDoc"]
+visualComplete = loaded_json["data"]["median"]["firstView"]["visualComplete"]
+requestsFull = loaded_json["data"]["median"]["firstView"]["requestsFull"]
 
 statsd.gauge('wpt.median.firstView.TTFB', (TTFB))
+statsd.gauge('wpt.median.firstView.render', (render))
+statsd.gauge('wpt.median.firstView.SpeedIndex', (SpeedIndex))
+statsd.gauge('wpt.median.firstView.bytesInDoc', (bytesInDoc))
+statsd.gauge('wpt.median.firstView.visualComplete', (visualComplete))
+statsd.gauge('wpt.median.firstView.requestsFull', (requestsFull))
