b5fc81a8a65c4ac52610c66a4011d3512e487cab
Stephen Donner <stephen.donner@gmail.com>
2019-01-29 16:39:00 -0800

    Just beginning the effort to verify our Nightly builds are up-to-date

---
+++ test_fx_versions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+import json
+
+
+def get_jsonparsed_data(url):
+    """
+    Receive the content of ``url``, parse it as JSON and return the object.
+
+    Parameters
+    ----------
+    url : str
+
+    Returns
+    -------
+    dict
+    """
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+
+fx_json_url = ("https://product-details.mozilla.org/1.0/firefox_versions.json")
+print(get_jsonparsed_data(fx_json_url))
