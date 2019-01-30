e9750bb5ed01d26817652f0dd8f65264fbb4fd4f
Stephen Donner <stephen.donner@gmail.com>
2019-01-29 17:27:16 -0800

    More cleanup

--- send_to_telemetry.py
+++ send_to_telemetry.py
@@ -36,16 +36,15 @@ def main(path):
 
         # need to only partition if we have a space in fullBrowserString, e.g. "Firefox Nightly"
         if ' ' in fullBrowserString:
-            splitBrowserStrings = fullBrowserString.partition(" ")
+            splitBrowserStrings = fullBrowserString.partition(' ')
             uppercaseBrowserName = splitBrowserStrings[0]
-            print("Partitioned browser_name string (splitBrowserStrings) is: ", splitBrowserStrings[0])
+            print("Partitioned browser_name string is: ", splitBrowserStrings[0])
             lowercaseBrowserName = splitBrowserStrings[0].lower()
             browserName = lowercaseBrowserName
         else:
             browserName = fullBrowserString.lower()
 
         # construct 'channel'
-        print("Try to set 'channel', using splitBrowserStrings")
         if ' ' in fullBrowserString:
             channelName = splitBrowserStrings[2].lower()
         else:
@@ -53,7 +52,6 @@ def main(path):
 
         result = TestResult(
             appName=browserName.lower(),
-            # appName=sample["browser_name"],
             channel=channelName,
             connection=test["data"]["connectivity"].lower(),
             url=test["data"]["testUrl"],
@@ -63,7 +61,7 @@ def main(path):
             sessionState="noAuth",
             metrics=values,
             version=sample["browser_version"])
-        # print(asdict(result))
+
         print(json.dumps(asdict(result)))
         # with open(f"wpt-telemetry-{test['data']['id']}.json", "w") as f:
         #   json.dump(asdict(result), f)
